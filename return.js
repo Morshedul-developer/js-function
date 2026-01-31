32eb6625278a933f5a42ad02502598adc2f51615
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-01-31 18:56:08 +0600

    explored the return keyword

---
+++ return.js
@@ -0,0 +1,17 @@
+function sum(a,b){
+    const sum = a+b;
+    console.log(sum);
+}
+
+function sub(a,b){
+    const sub = a-b;
+    return sub;
+}
+
+const sumOfNumbers = sum(5,5);
+console.log(sumOfNumbers);
+
+console.log("-------------------------");
+
+const subOfNumbers = sub(10,5);
+console.log(subOfNumbers);
